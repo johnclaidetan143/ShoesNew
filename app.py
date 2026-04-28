b79d3fa4d9370c3e141f07aa1a5953ecac71712d
johnclaidetan143 <johnclaidetan143>
2026-04-28 11:17:31 +0800

    update project files

--- app.py
+++ app.py
@@ -774,6 +774,11 @@ def page_not_found(error):
     return render_template("404.html"), 404
 
 
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template("500.html"), 500
+
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
